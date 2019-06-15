c88ba550b16a049ed54814a2b029390e4401c2c1
Jsmith64 <50225482+Jsmith64@users.noreply.github.com>
2019-06-15 18:33:09 -0500

    Add files via upload

---
+++ FizzBuzz/TestFile.java
@@ -0,0 +1,8 @@
+
+public class TestFile {
+    public static void main(String[] args){
+        FizzBuzz iD = new FizzBuzz();
+        String answer = iD.fizzBuzz(14);
+        System.out.println(answer);
+    }
+}
